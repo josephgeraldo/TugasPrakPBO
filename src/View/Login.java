6a0c2a029ada4af87da6cbcb1dee931739cb6de1
Joseph Geraldo <>
2022-11-21 21:05:00 +0700

    commit

--- src/View/Login.java
+++ src/View/Login.java
@@ -10,11 +10,9 @@ import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
 
 
 /**

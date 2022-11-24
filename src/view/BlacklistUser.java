22ab135d1cf5ee6ecf6212f9d7178a3bd9ff0f4f
Cent <Cent@DESKTOP-CM1KUG1.mshome.net>
2022-11-24 21:10:15 +0700

    update menu admin

--- src/view/BlacklistUser.java
+++ src/view/BlacklistUser.java
@@ -13,12 +13,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -26,12 +26,15 @@ import javax.swing.table.DefaultTableModel;
  */
 public class BlacklistUser {
     JButton button1,button2;
-    JLabel label;
+    JLabel label,label1,label2,label3,label4,label5,label6,label7,label8,label9,label10;
     JTextField textField;
     JFrame frame,frame1;
     JTable table;
     String from;
-
+    JScrollPane sp;
+    public static void main(String[] args) {
+        new BlacklistUser();
+    }
     public BlacklistUser(){
         frame = new JFrame("Blacklist User");
         frame.setSize(320,200);
@@ -42,7 +45,7 @@ public class BlacklistUser {
         textField.setBounds(137,5, 140, 35);
         button1 = new JButton("Blacklist");
         button1.setBounds(108,60,90,35);
-        button2 = new JButton("Show List");
+        button2 = new JButton("Show User");
         button2.setBounds(108,100,90,20);
         button1.setCursor(new Cursor(Cursor.HAND_CURSOR));
         button2.setCursor(new Cursor(Cursor.HAND_CURSOR));
@@ -54,7 +57,7 @@ public class BlacklistUser {
                     Class.forName("com.mysql.cj.jdbc.Driver");
                     String blackList = "Blacklist";
                     Connection con = DriverManager.getConnection("jdbc:mysql://localhost/tokokue","root","");
-                    String sql = "UPDATE user SET tipe = '%"+blackList+"%' where username = '%"+textField.getText()+"%'";
+                    String sql = "UPDATE user SET tipe = '"+blackList+"' where username = '"+textField.getText()+"'";
                     PreparedStatement stm = con.prepareStatement(sql);
                     stm.executeUpdate(sql);
                     JOptionPane.showMessageDialog(null, "Username Berhasil Di Blacklist");
@@ -70,53 +73,80 @@ public class BlacklistUser {
         button2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae){
-                String columnNames[] = {"id_user","username","password","no_telepon","nama_lengkap","alamat_lengkap","kota","provinsi","kode_post","tipe"};
-                frame1 = new JFrame("List User");
-                frame1.setSize(1000,400);
-                frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                DefaultTableModel model = new DefaultTableModel();
-                model.setColumnIdentifiers(columnNames);
-                table = new JTable();
-                table.setModel(model);
-                table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-                table.setFillsViewportHeight(true);
-                JScrollPane scroll = new JScrollPane(table);
-                scroll.setHorizontalScrollBarPolicy(
-                        JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-                scroll.setVerticalScrollBarPolicy(
-                        JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-                String idUser,uname,pass,noTelp,namaLengkap,alamat,kota,prov,kodePos,tipeAcc;
                 try{
+                    frame1 = new JFrame("USER "+textField.getText());
+                    frame1.setSize(400,470);
                     Class.forName("com.mysql.cj.jdbc.Driver");
                     Connection con = DriverManager.getConnection("jdbc:mysql://localhost/tokokue","root","");
-                    String sql = "SELECT * from user";
-                    PreparedStatement stm = con.prepareStatement(sql);
-                    ResultSet rs = stm.executeQuery();
+                    String sql = "SELECT * from user where username = '"+textField.getText()+"'";
+                    Statement stm = con.createStatement();
+                    ResultSet rs = stm.executeQuery(sql);
                     int i = 0;
-                    while(rs.next()){
-                        idUser = rs.getString("id_user");
+                    int id = 0;
+                    String uname;
+                    String pass;
+                    String telepon;
+                    String name;
+                    String address;
+                    String city;
+                    String prov;
+                    String pos;
+                    String type;
+                    if(rs.next()){
+                        i+=1;
+                        if(i == 1){
+                            JOptionPane.showMessageDialog(null, "USER KETEMU!!!");
+                        }
+                        id = rs.getInt("id_user");
                         uname = rs.getString("username");
                         pass = rs.getString("password");
-                        noTelp = rs.getString("no_telepon");
-                        namaLengkap = rs.getString("nama_lengkap");
-                        alamat = rs.getString("alamat_lengkap");
-                        kota = rs.getString("kota");
+                        telepon = rs.getString("no_telepon");
+                        name = rs.getString("nama_lengkap");
+                        address = rs.getString("alamat_lengkap");
+                        city = rs.getString("kota");
                         prov = rs.getString("provinsi");
-                        kodePos = rs.getString("kode_post");
-                        tipeAcc = rs.getString("tipe");
-                        model.addRow(new Object[]{idUser,uname,pass,noTelp,namaLengkap,alamat,kota,prov,kodePos,tipeAcc});
-                        i++;
-                    }
-                    if (i < 1) {
-                        JOptionPane.showMessageDialog(null, "No Record Found", "Error", JOptionPane.ERROR_MESSAGE);
+                        pos = rs.getString("kode_post");
+                        type = rs.getString("tipe");  
+                        
+                        label1 = new JLabel("ID                    = "+id);
+                        label1.setBounds(50,25,250,35);
+                        label2 = new JLabel("Username    = "+uname);
+                        label2.setBounds(50,60,250,35);
+                        label3 = new JLabel("Password    = "+pass);
+                        label3.setBounds(50,95,250,35);
+                        label4 = new JLabel("No Telp        = "+telepon);
+                        label4.setBounds(50,130,250,35);
+                        label5 = new JLabel("Nama           = "+name);
+                        label5.setBounds(50,165,250,35);
+                        label6 = new JLabel("Alamat         = "+address);
+                        label6.setBounds(50,200,250,35);
+                        label7 = new JLabel("Kota              = "+city);
+                        label7.setBounds(50,235,250,35);
+                        label8 = new JLabel("Provinsi       = "+prov);
+                        label8.setBounds(50,270,250,35);
+                        label9 = new JLabel("Kode Pos      = "+pos);
+                        label9.setBounds(50,305,250,35);
+                        label10 = new JLabel("Tipe Acc       = "+type);
+                        label10.setBounds(50,340,250,35);
+                        
+                        frame1.add(label1);
+                        frame1.add(label2);
+                        frame1.add(label3);
+                        frame1.add(label4);
+                        frame1.add(label5);
+                        frame1.add(label6);
+                        frame1.add(label7);
+                        frame1.add(label8);
+                        frame1.add(label9);
+                        frame1.add(label10);
+                        frame1.setLayout(null);
+                        frame1.setVisible(true);
+                    }else{
+                        JOptionPane.showMessageDialog(null, "USER TIDAK ADA!!!");
                     }
                 } catch (Exception ex) {
                     JOptionPane.showMessageDialog(null, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
                 }
-                
-                frame1.add(scroll);
-                frame1.setLayout(null);
-                frame1.setVisible(true);
             }
         });
         

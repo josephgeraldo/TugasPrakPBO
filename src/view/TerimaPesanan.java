6dfae4049a2556b1aca3978c4c7baf24876978df
Joseph Geraldo <>
2022-11-25 07:42:14 +0700

    commit

--- src/view/TerimaPesanan.java
+++ src/view/TerimaPesanan.java
@@ -40,78 +40,38 @@ public class TerimaPesanan {
         PengirimanEnum antar = PengirimanEnum.valueOf("DIANTAR");
         PengirimanEnum sampai = PengirimanEnum.valueOf("SAMPAI");
 
-        String columns[] = {"ID Brg", "Alamat", "Berat", "Jumlah", "Ukuran", "Warna", "Hrg Tot", "B.Kirim", "J.Bayar",
-            "S.Bayar", "S.Kirim"};
+        String columns[] = {"IDPesanan", "IDProduk", "IDUser", "Berat", "Harga"};
 
         DefaultTableModel model = new DefaultTableModel(null, columns);
         JTable table = new JTable(model);
         table.setShowGrid(true);
         table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         TableColumn eventColumn = table.getColumnModel().getColumn(0);
-        eventColumn.setPreferredWidth(50);
+        eventColumn.setPreferredWidth(100);
         TableColumn eventColumn2 = table.getColumnModel().getColumn(1);
-        eventColumn2.setPreferredWidth(150);
+        eventColumn2.setPreferredWidth(100);
         TableColumn eventColumn3 = table.getColumnModel().getColumn(2);
-        eventColumn3.setPreferredWidth(50);
+        eventColumn3.setPreferredWidth(100);
         TableColumn eventColumn4 = table.getColumnModel().getColumn(3);
-        eventColumn4.setPreferredWidth(50);
+        eventColumn4.setPreferredWidth(100);
         TableColumn eventColumn5 = table.getColumnModel().getColumn(4);
-        eventColumn5.setPreferredWidth(70);
-        TableColumn eventColumn6 = table.getColumnModel().getColumn(5);
-        eventColumn6.setPreferredWidth(70);
-        TableColumn eventColumn7 = table.getColumnModel().getColumn(6);
-        eventColumn7.setPreferredWidth(50);
-        TableColumn eventColumn8 = table.getColumnModel().getColumn(7);
-        eventColumn8.setWidth(50);
-        TableColumn eventColumn9 = table.getColumnModel().getColumn(8);
-        eventColumn9.setWidth(50);
-        TableColumn eventColumn10 = table.getColumnModel().getColumn(9);
-        eventColumn10.setWidth(40);
+        eventColumn5.setPreferredWidth(100);
         JScrollPane pane = new JScrollPane(table);
 
         conn.connect();
-        String query = "SELECT pesanan.barang_id, alamat.alamat, barang.berat, pesanan.jumlah, pesanan.ukuran, pesanan.warna, pesanan.harga_total, pesanan.biaya_pengiriman, pesanan.jenis_pembayaran, pesanan.status_pembayaran, pesanan.status_pengiriman FROM alamat INNER JOIN pesanan ON alamat.alamat_id = pesanan.pesanan_id INNER JOIN barang ON pesanan.barang_id = barang.barang_id";
+        String query = "SELECT id_pesanan, id_produk, id_user, berat, jumlah, harga FROM pesanan";
         try {
             Statement stmt = conn.con.createStatement();
             ResultSet rs = stmt.executeQuery(query);
-
             while (rs.next()) {
-                String barang = rs.getString("barang_id");
-                String alamat = rs.getString("alamat");
+                String id_pesanan = rs.getString("id_pesanan");
+                String id_produk = rs.getString("id_produk");
+                String id_user = rs.getString("id_user");
                 String berat = rs.getString("berat");
                 String jumlah = rs.getString("jumlah");
-                String ukuran = rs.getString("ukuran");
-                String warna = rs.getString("warna");
-                String hargaT = rs.getString("harga_total");
-                String biayaP = rs.getString("biaya_pengiriman");
-                String jns_bayar = rs.getString("jenis_pembayaran");
+                String harga = rs.getString("harga");
 
-                if (jns_bayar.equals(0)) {
-                    jns_bayar = "Transfer Bank";
-                } else if (jns_bayar.equals(1)) {
-                    jns_bayar = "COD";
-                } else if (jns_bayar.equals(2)) {
-                    jns_bayar = "OVO";
-                } else if (jns_bayar.equals(3)) {
-                    jns_bayar = "GOPAY";
-                } else if (jns_bayar.equals(4)) {
-                    jns_bayar = "DANA";
-                }
-
-                String status_bayar = rs.getString("status_pembayaran");
-
-                String status_kirim = rs.getString("status_pengiriman");
-                if (status_kirim.equals(kemas.ordinal())) {
-                    kemas = PengirimanEnum.PENGEMASAN;
-                } else if (status_kirim.equals(tunggu.ordinal())) {
-                    tunggu = PengirimanEnum.MENUNGGU_KURIR;
-                } else if (status_kirim.equals(antar)) {
-                    antar = PengirimanEnum.MENUNGGU_KURIR;
-                } else if (status_kirim.equals(sampai)) {
-                    sampai = PengirimanEnum.SAMPAI;;
-                }
-
-                String[] baris = {barang, alamat, berat, jumlah, ukuran, warna, hargaT, biayaP, jns_bayar, status_bayar, status_kirim};
+                String[] baris = {id_pesanan, id_produk, id_user, berat, jumlah, harga};
                 model.addRow(baris);
             }
 
@@ -124,8 +84,9 @@ public class TerimaPesanan {
 
         JButton buttonUpdate = new JButton("AMBIL");
         buttonUpdate.setBounds(110, 460, 100, 40);
-
+        
         panel.add(pane);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(buttonUpdate);
         frame.add(panel);
         frame.add(panel);
@@ -135,6 +96,7 @@ public class TerimaPesanan {
         buttonUpdate.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {   
+                
             }
         });
     }

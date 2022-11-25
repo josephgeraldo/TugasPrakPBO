0804635e46407f362c6dd1ee1163a9a754a648c1
Vincent Benedict Mars W <114089447+vincentbmw@users.noreply.github.com>
2022-11-25 11:59:54 +0700

    Update AturPesanan

--- src/view/AturPesanan.java
+++ src/view/AturPesanan.java
@@ -19,32 +19,31 @@ import java.util.logging.Logger;
  * @author Cent
  */
 public class AturPesanan {
-    JLabel label,label1,label2,label3;
-    JTextField textField,textField1,textField2,textField3;
+    JLabel label,label1,label2;
+    JTextField textField,textField1,textField2;
     JButton button,button1;
     JFrame frame;
+    public static void main(String[] args) {
+        new AturPesanan();
+    }
     public AturPesanan(){
         frame = new JFrame("Atur Pesanan");
         frame.setSize(320,270);
         frame.getContentPane().setBackground(Color.lightGray);
-        label = new JLabel("Id Pesanan      :");
+        label = new JLabel("Id Pesanan                  :");
         label.setBounds(5,5,130,35);
         textField = new JTextField();
         textField.setBounds(135,5, 140, 35);
-        label1 = new JLabel("Nama Customer      :");
+        label1 = new JLabel("Status Bayar               :");
         label1.setBounds(5,50,130,35);
-        textField1 = new JTextField();
+        textField1 = new JTextField(); 
         textField1.setBounds(135,50, 140, 35);
-        label2 = new JLabel("Alamat Customer    :");
+        label2 = new JLabel("Status Pengiriman    :");
         label2.setBounds(5,100,130,35);
         textField2 = new JTextField();
         textField2.setBounds(135,100, 140, 35);
-        label3 = new JLabel("Harga Barang           :");
-        label3.setBounds(5,150,130,35);
-        textField3 = new JTextField();
-        textField3.setBounds(135,150, 140, 35);
         button = new JButton("Submit");
-        button.setBounds(115,190,80,35);
+        button.setBounds(115,150,80,35);
         button.setCursor(new Cursor(Cursor.HAND_CURSOR));
         button1 = new JButton("<");
         button1.setBounds(4,215,45,15);
@@ -53,22 +52,20 @@ public class AturPesanan {
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae){
-                if (textField.getText().isEmpty() || textField1.getText().isEmpty() || textField2.getText().isEmpty() || textField3.getText().isEmpty()) {
+                if (textField.getText().isEmpty() || textField1.getText().isEmpty() || textField2.getText().isEmpty()) {
                     JOptionPane.showMessageDialog(null, "Semua kolom harus diisi!");
                 }else{
                     try {
                         Class.forName("com.mysql.cj.jdbc.Driver");
-                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/tokokue","root","");
-                        Statement stm = con.createStatement();
-                        int cv = Integer.parseInt(textField3.getText());
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/tokokue2","root","");
                         int id = Integer.parseInt(textField.getText());
-                        String sql = "INSERT INTO pesanan VALUES("+id+",'"+textField1.getText()+"','"+textField2.getText()+"','"+cv+"',curdate())";
+                        Statement stm = con.createStatement();
+                        String sql = "UPDATE pesanan SET status_pembayaran = '"+textField1.getText()+"', status_pengiriman = '"+textField2.getText()+"' where pesanan_id = '"+id+"'";
                         stm.executeUpdate(sql);
                         textField.setText("");
                         textField1.setText("");
                         textField2.setText("");
-                        textField3.setText("");
-                        JOptionPane.showMessageDialog(null, "Data Pesanan Berhasil Di Input");
+                        JOptionPane.showMessageDialog(null, "Data Pesanan Berhasil Di Atur");
                         con.close();
                     }catch (SQLException se){
                         se.printStackTrace();
@@ -83,11 +80,9 @@ public class AturPesanan {
         frame.add(label);
         frame.add(label1);
         frame.add(label2);
-        frame.add(label3);
         frame.add(textField);
         frame.add(textField1);
         frame.add(textField2);
-        frame.add(textField3);
         frame.add(button);
         frame.add(button1);
         frame.setLayout(null);
